8763ae57661b078b6e61d17dea52e489250f45ee
Shivang <daveshivang15@gmail.com>
2021-04-20 15:45:28 -0500

    Add item model files..

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_042628) do
+ActiveRecord::Schema.define(version: 2021_04_20_044259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_v1_items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "api_v1_users", force: :cascade do |t|
     t.string "first_name"
