be2d7cfd3b732eed9a1816259e65c3edfbbd10d1
Shivang <daveshivang15@gmail.com>
2021-04-22 20:49:13 -0500

    Added user-<orders-<items association..

--- app/models/api/v1/order.rb
+++ app/models/api/v1/order.rb
@@ -1,4 +1,6 @@
 class Api::V1::Order < ApplicationRecord
   has_many :order_items, class_name: 'Api::V1::OrderItem'
   has_many :items, through: :order_items, class_name: 'Api::V1::Item'
+
+  belongs_to :user, class_name: 'Api::V1::User'
 end
