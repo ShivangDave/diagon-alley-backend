9eabc264d229d26d8fc2b1498ac9e61077b93d99
Shivang <daveshivang15@gmail.com>
2021-04-24 12:44:22 -0500

    Added items controller # show method..

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,24 @@
 class Api::V1::ItemsController < ApplicationController
 
   # before_action :authorized
+  before_action :find_item, only: [:show]
 
   def index
     @items = Api::V1::Item.all
     render :json => Api::V1::ItemSerializer.new(@items).serializable_hash, :status => :ok
+  end
+
+  def show
+    if @item
+      render :json => Api::V1::ItemSerializer.new(@item).serializable_hash, :status => :ok
+    else
+      render :json => { "error": "Item not found.." }, :status => :not_found
+    end
+  end
+
+  private
+  def find_item
+    @item = Api::V1::Item.find_by(id: params[:id])
   end
 
 end
