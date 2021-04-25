397153b579a1bfc9daecf44dd0e26215fc2674c0
Shivang <daveshivang15@gmail.com>
2021-04-25 16:33:59 -0500

    Add offers option..

--- app/models/api/v1/item.rb
+++ app/models/api/v1/item.rb
@@ -4,4 +4,8 @@ class Api::V1::Item < ApplicationRecord
 
   has_many :item_images, class_name: 'Api::V1::ItemImage'
   validates :name, :description, :price, presence: true
+
+  def original_price
+    (self.price / 10).round * 20 + 1.99
+  end
 end
--- app/serializers/api/v1/item_serializer.rb
+++ app/serializers/api/v1/item_serializer.rb
@@ -1,4 +1,5 @@
 class Api::V1::ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :price, :item_images
+  attributes :id, :name, :description, :price, :item_images, :original_price
+
 end
