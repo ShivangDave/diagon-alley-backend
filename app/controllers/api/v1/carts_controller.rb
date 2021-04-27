117f2aa74ddd01356784c5adc36901726de17e8d
Shivang <daveshivang15@gmail.com>
2021-04-27 11:45:36 -0500

    Added index method for cart..

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::CartsController < ApplicationController
 
   before_action :authorized, only: [:create]
 
+  def index
+    @cart_items = current_user.cart.items
+    render :json => @cart_items, include: [:item_images], :status => :ok
+  end
+
   def create
     @cart_items = current_user.cart.add_item(cart_params[:id])
     render :json => @cart_items, :status => :ok
