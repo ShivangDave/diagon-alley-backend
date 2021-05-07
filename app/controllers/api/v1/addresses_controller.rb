b5a5cc199316135f4426de3cfd2c4555cc9735cc
Shivang <daveshivang15@gmail.com>
2021-05-07 14:33:51 -0500

    Edit address method..

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AddressesController < ApplicationController
+
+  def update
+    byebug
+  end
+
+  private
+  def address_params
+    params.require(:address).permit()
+  end
+
 end
